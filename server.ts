2c02083503c2bb45225498a314731b62afb28b11
dCOMMICS <sanwurld@gmail.com>
2023-09-22 21:45:26 +0300

    🤞

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import { app } from "./app";
 
 require("./.env").config();
 
+
+
 // create server
 // app.listen(process.env.PORT || 3000, () => {
 //   console.log("Server is running on port 3000");
